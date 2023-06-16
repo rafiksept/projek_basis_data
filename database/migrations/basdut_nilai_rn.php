29902b5fe283e17c3ffb43f4e05faa194f9631bd
Juan Alberto Galih Leo <91479427+juanalberto02@users.noreply.github.com>
2023-06-16 22:26:53 +0700

    Add files via upload

---
+++ database/migrations/basdut_nilai_rn.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('basdut_nilai_rn', function (Blueprint $table) {
+            $table->id();
+            $table->string('Kode_Matkul');
+            $table->string('NIM');
+            $table->integer('Nilai');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+    }
+};
