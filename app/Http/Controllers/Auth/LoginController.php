80bb1401472736c34601be08e1e1ec812536d804
Ridhok <rdhpndh@gmail.com>
2023-06-17 11:22:22 +0700

    fixing logincontrollers after importing auth package

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,38 +3,63 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function showLoginForm()
     {
-        $this->middleware('guest')->except('logout');
+        return view('auth.login');
+    }
+
+    // public function login(Request $request)
+    // {
+    //     $credentials = $request->all();
+    //     dd($credentials);
+    //     if (Auth::attempt($credentials)) {
+    //         // dd('haruse bener');
+    //         // Authentication successful, redirect user to a protected page
+    //         return redirect()->intended('/');
+    //     } else {
+    //         // dd('haruse salah');
+    //         // Invalid credentials, redirect back to the login page with an error
+    //         return redirect()->route('login')->withErrors(['message' => 'salah, coba ulang']);
+    //     }
+    // }
+
+
+    public function login(Request $request)
+{
+    $credentials = $request->only('password');
+    
+    // Check if the input contains '@'
+    if (strpos($request->input('email'), '@') !== false) {
+        // If '@' is present, search for 'email' field
+        $credentials['email'] = $request->input('email');
+    } else {
+        // If '@' is not present, search for 'nik' field
+        $credentials['nik'] = $request->input('email');
+        unset($credentials['email']); // Remove the 'email' key from credentials
+    }
+
+    if (Auth::attempt($credentials)) {
+        // Authentication successful, redirect user to a protected page
+        return redirect()->intended('/');
+    } else {
+        // Invalid credentials, redirect back to the login page with an error
+        return redirect()->route('login')->withErrors(['message' => 'Invalid credentials, please try again']);
+    }
+}
+
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return redirect('/login');
     }
 }
