d1e5ade34df3faee6caf8eb35f233663f3078a18
Ridhok <rdhpndh@gmail.com>
2023-06-17 14:19:00 +0700

    adding rules so that unverified users cant be logged in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,40 +6,42 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-
-
 class LoginController extends Controller
 {
-
     public function showLoginForm()
     {
         return view('auth.login');
     }
 
-
     public function login(Request $request)
-{
-    $credentials = $request->only('password');
-    
-    // Check if the input contains '@'
-    if (strpos($request->input('email'), '@') !== false) {
-        // If '@' is present, search for 'email' field
-        $credentials['email'] = $request->input('email');
-    } else {
-        // If '@' is not present, search for 'nik' field
-        $credentials['nik'] = $request->input('email');
-        unset($credentials['email']); // Remove the 'email' key from credentials
-    }
+    {
+        $credentials = $request->only('password');
 
-    if (Auth::attempt($credentials)) {
-        // Authentication successful, redirect user to a protected page
-        return redirect()->intended('/');
-    } else {
-        // Invalid credentials, redirect back to the login page with an error
-        return redirect()->route('login')->withErrors(['message' => 'Invalid credentials, please try again']);
-    }
-}
+        // Check if the input contains '@'
+        if (strpos($request->input('email'), '@') !== false) {
+            // If '@' is present, search for 'email' field
+            $credentials['email'] = $request->input('email');
+        } else {
+            // If '@' is not present, search for 'nik' field
+            $credentials['nik'] = $request->input('email');
+            unset($credentials['email']); // Remove the 'email' key from credentials
+        }
 
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            if ($user->is_verified) {
+                // Authentication successful and user is verified, redirect user to a protected page
+                return redirect()->intended('/');
+            } else {
+                // User is not verified, show error message
+                return redirect()->route('login')->withErrors(['message' => 'Please verify your email first!']);
+            }
+        } else {
+            // Invalid credentials, redirect back to the login page with an error
+            return redirect()->route('login')->withErrors(['message' => 'Invalid credentials, please try again']);
+        }
+    }
 
     public function logout()
     {
