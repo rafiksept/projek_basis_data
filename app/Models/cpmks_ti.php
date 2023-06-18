865b302da4d754af4ffd51d8036b7ed9524ab325
Gede Nayaka Baswara <91718331+NykBswr@users.noreply.github.com>
2023-06-18 17:32:29 +0800

    Update cpmks_ti

--- app/Models/cpmks_ti.php
+++ app/Models/cpmks_ti.php
@@ -7,5 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class cpmks_ti extends Model
 {
+    protected $fillable = [
+        'Mata_Kuliah',
+        'S1',
+        'S2',
+        'S3',
+        'S4',
+        'S5',
+        'S6',
+        'S7',
+        'S8',
+        'S9',
+        'S10',
+        'S11',
+        'KU1',
+        'KU2',
+        'KU3',
+        'KU4',
+        'KU5',
+        'KU6',
+        'KU7',
+        'KU8',
+        'KU9',
+        'P1',
+        'P2',
+        'P3',
+        'KK1',
+        'KK2',
+        'KK3',
+        'KK4',
+        'KK5',
+        'KK6',
+        'KK7',
+        'KK8',
+        'KK9',
+        'KK10',
+    ];
     use HasFactory;
 }
